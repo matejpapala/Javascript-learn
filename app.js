02dae9b2adbb100c15d679dddceed3181e7fda61
matejpapala <123272402+matejpapala@users.noreply.github.com>
2023-03-23 12:04:53 +0100

    1 file modified

--- app.js
+++ app.js
@@ -3,8 +3,24 @@ import express from 'express';
 const app = express();
 
 app.get('/', (req, res) => {
-  const date = new Date();
-  res.json({ date });
+  res.write('<!DOCTYPE html>');
+  res.write('<html lang = "cs">');
+  res.write('<head>');
+  res.write('<meta charset = "UTF-8">');
+  res.write('</head>');
+  res.write('<body>');
+  res.write('<h1>nesnasim SQL</h1>');
+  res.write('<a href = "/stats">Statistika</a>');
+  res.write('</body>');
+  res.write('</html>');
+  res.end();
+});
+
+app.get('/stats', (req, res) => {
+  res.json({
+    who: 'Matej',
+    gpa: 4.21,
+  });
 });
 
 app.listen(3000, () => {
