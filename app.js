d61f15c4102efc0509335a15063ebe183818c3c5
matejpapala <123272402+matejpapala@users.noreply.github.com>
2023-03-23 13:56:17 +0100

    5 files modified, 1 added, 1 deleted

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import axios from 'axios';
 
 const app = express();
 
@@ -15,6 +16,8 @@ app.get('/', (req, res) => {
   res.write('<a href = "/stats">Statistika</a>');
   res.write('<br>');
   res.write('<a href = "/static/index.html">Ustav</a>');
+  res.write('<br>');
+  res.write('<a href = "myprofile">jo</a>');
   res.write('</body>');
   res.write('</html>');
   res.end();
@@ -25,6 +28,32 @@ app.get('/stats', (req, res) => {
     who: 'Matej',
     gpa: 1.73,
   });
+});
+
+app.get('/profile', async (req, res) => {
+  const gh = await axios.get('https://api.github.com/users/matejpapala');
+  res.json(gh.data);
+});
+
+app.get('/myprofile', async (req, res) => {
+  const gh = await axios.get('https://api.github.com/users/matejpapala');
+  const user = gh.data;
+  const date = new Date();
+  res.write('<!DOCTYPE html>');
+  res.write('<html lang = "cs">');
+  res.write('<head>');
+  res.write('<meta charset = "UTF-8">');
+  res.write('</head>');
+  res.write('<body>');
+  res.write(`<h1>${user.name}</h1>`);
+  res.write('<br>');
+  res.write(`<h1 style = "font-size: 22px">${user.blog}</h1>`);
+  res.write('<br>');
+  res.write(`<img id = "obrazek" src = "${user.avatar_url}">`);
+  /*res.write(`<h1>jsem clenem githubu ${user.created_at - date}</h1>`);*/
+  res.write('</body>');
+  res.write('</html>');
+  res.end();
 });
 
 app.listen(3000, () => {
---
+++ static/index.js
@@ -0,0 +1,7 @@
+/*const namePrompt = function () {
+  let username = window.prompt('Ahoj, jak se jmenujes?');
+  document.getElementById('nadpis').innerHTML = 'Ahoj ' + username;
+};
+
+namePrompt();
+*/
